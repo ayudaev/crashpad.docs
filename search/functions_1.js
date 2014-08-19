4a66b4908fcedc41a38dae10dd239a3de7daa8df
Mark Mentovai <mark@chromium.org>
2014-08-18 20:42:23 -0700

    Update documentation to correspond to Crashpad 280c4345c5f7.

--- search/functions_1.js
+++ search/functions_1.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['base',['base',['../classcrashpad_1_1CheckedRange.html#a7a1eed88055c8780fce3acaf9699bb76',1,'crashpad::CheckedRange']]],
+  ['bootstrapcheckin',['BootstrapCheckIn',['../namespacecrashpad.html#ad34c73bd6ad5d4c9aff2d68b7f26b4c3',1,'crashpad']]],
   ['bootstraperrormessage',['BootstrapErrorMessage',['../namespacecrashpad_1_1test.html#a8db2fa97998b00a8ee57398fa8c8b14e',1,'crashpad::test']]]
 ];
